0f9521b06b1b4680fc7ae34f0cd7c0ed0a36dc32
Bill Wu <billwu@mit.edu>
2020-07-31 16:53:06 -0400

    greyscale

--- detector.py
+++ detector.py
@@ -2,5 +2,6 @@ import cv2
 import numpy as np
 
 img = cv2.imread('input.png')
-cv2.imshow('input', img)
-cv2.waitKey(0)
+lane_img = np.copy(img)  # Need a copy to avoid aliasing issues mutating og image
+greyscale = cv2.cvtColor(lane_img, cv2.COLOR_RGB2GRAY)
+
